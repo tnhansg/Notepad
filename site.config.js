e79cadca7cf1d8070d83018c1ab682cfbb261fc2
Nguyễn Thiện Nhân <88951651+tnhansg@users.noreply.github.com>
2023-07-22 02:27:27 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,20 +12,20 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Thien Nhan`,
+      href: "https://facebook.com/tnhantl",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Thien Nhan Note",
+    description: "welcome to Thien Nhan Note",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://notepad.thiennhan.gq",
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
